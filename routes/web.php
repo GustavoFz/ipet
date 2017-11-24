8d8153fe6cdf9b5d7cc6e29b86e26fb412c11434
Andrhé Victor <andrhevictor@gmail.com>
2017-11-24 09:30:36 -0400

    apelido das rotas
    Apeido seguindo o padrão:
    "ENTIDADE"."AÇÂO"

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,26 @@
 Route::get('/', function () {return view('welcome');} );
 
 /*ROTAS DE USUÁRIO*/
-Route::post('/user/save',['uses'=>'UserController@save']);
-Route::get('/user/create',['uses'=>'UserController@create']);
-Route::get('/users',['uses'=>'UserController@show']);
-Route::get('/user/show/{id}',['uses'=>'UserController@showOne']);
-Route::post('/user/update/{id}',['uses'=>'UserController@update']);
-Route::get('/user/delete/{id}',['uses'=>'UserController@delete']);
+Route::post('/user/save',['as'=>'user.save','uses'=>'UserController@save']);
+Route::get('/user/create',['as'=>'user.create','uses'=>'UserController@create']);
+Route::get('/users',['as'=>'users','uses'=>'UserController@show']);
+Route::get('/user/show/{id}',['as'=>'user.show.one','uses'=>'UserController@showOne']);
+Route::post('/user/update/{id}',['as'=>'user.update','uses'=>'UserController@update']);
+Route::get('/user/delete/{id}',['as'=>'user.delete','uses'=>'UserController@delete']);
 
 /*ROTAS DE ANIMAL*/
-Route::post('/animal/save',['uses'=>'AnimalController@save']);
-Route::get('/animal/create',['uses'=>'AnimalController@create']);
-Route::get('/animals',['uses'=>'AnimalController@show']);
-Route::get('/animal/show/{id}',['uses'=>'AnimalController@showOne']);
-Route::post('/animal/update/{id}',['uses'=>'AnimalController@update']);
-Route::get('/animal/delete/{id}',['uses'=>'AnimalController@delete']);
+Route::post('/animal/save',['as'=>'animal.save','uses'=>'AnimalController@save']);
+Route::get('/animal/create',['as'=>'animal.create','uses'=>'AnimalController@create']);
+Route::get('/animals',['as'=>'animals','uses'=>'AnimalController@show']);
+Route::get('/animal/show/{id}',['as'=>'animal.show.one','uses'=>'AnimalController@showOne']);
+Route::post('/animal/update/{id}',['as'=>'animal.update','uses'=>'AnimalController@update']);
+Route::get('/animal/delete/{id}',['as'=>'animal.delete','uses'=>'AnimalController@delete']);
 
 /*ROTAS DE SERVICO*/
-Route::post('/servico/save',['uses'=>'ServicoController@save']);
-Route::get('/servico/create',['uses'=>'ServicoController@create']);
-Route::get('/servicos',['uses'=>'ServicoController@show']);
-Route::get('/servico/show/{id}',['uses'=>'ServicoController@showOne']);
-Route::post('/servico/update/{id}',['uses'=>'ServicoController@update']);
-Route::get('/servico/delete/{id}',['uses'=>'ServicoController@delete']);
+Route::post('/servico/save',['as'=>'servico.save','uses'=>'ServicoController@save']);
+Route::get('/servico/create',['as'=>'servico.create','uses'=>'ServicoController@create']);
+Route::get('/servicos',['as'=>'servicos','uses'=>'ServicoController@show']);
+Route::get('/servico/show/{id}',['as'=>'servico.show.one','uses'=>'ServicoController@showOne']);
+Route::post('/servico/update/{id}',['as'=>'servico.update','uses'=>'ServicoController@update']);
+Route::get('/servico/delete/{id}',['as'=>'servico.delete','uses'=>'ServicoController@delete']);
 
