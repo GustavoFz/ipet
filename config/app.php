e9fa64d8688194b78d037a2121aa112115d89866
Andrhe Victor <andrhevictor@gmail.com>
2017-12-02 14:55:56 -0400

    bouncer

--- config/app.php
+++ config/app.php
@@ -141,6 +141,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Silber\Bouncer\BouncerServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -191,7 +192,7 @@ return [
     */
 
     'aliases' => [
-
+        'Bouncer' => Silber\Bouncer\BouncerFacade::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
