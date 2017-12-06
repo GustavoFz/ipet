86a729c1a90e0b6be45053b4d89a559c23f6c70a
Andrhé Victor <andrhevictor@gmail.com>
2017-12-06 12:11:53 -0400

    teste

--- resources/views/servicos.blade.php
+++ resources/views/servicos.blade.php
@@ -5,6 +5,38 @@
     </a>
 </div>
 
+<div class="row">
+    <div class="col s8">
+        @if(Auth::user()->servicos()->count() > 0)
+        <ul class="collapsible" data-collapsible="accordion">
+
+            @foreach(Auth::user()->servicos as $servico)
+            <li>
+            <div class="collapsible-header"><i class="material-icons">pets</i>{{$servico->status}}</div>
+                <div class="collapsible-body">
+                    <span><b>Nome: </b>{{$animal->nome}}</span> <br>
+                    <span><b>Idade: </b>{{$animal->idade}}</span> <br>
+                    <span><b>Espécie: </b>{{$animal->especie}}</span> <br>
+                    @if($animal->servicos->isNotEmpty())
+                        <span><b>Serviços: </b> Esse animal já foi atendido {{$animal->servicos->count()}} vezes</span> <br>
+                        @forelse($animal->servicos as $servico)
+                            <ul class="collection">
+                              <li class="collection-item">{{$loop->iteration}} / </li>
+                            </ul>
+                        @empty
+                            Não há servicos
+                        @endforelse
+                    @else
+                        Não há serviços
+                    @endif
+                </div>
+            </li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
+</div>
+
 <!-- /Botão add -->
 @if(Auth::user()->servicos()->count() > 0)
     Serviços: <br>
@@ -36,3 +68,10 @@
     <br>
     Serviços: Não há serviços<br>
 @endif
+
+
+  <script type="text/javascript">
+      $(document).ready(function(){
+        $('.collapsible').collapsible();
+      });
+  </script>
